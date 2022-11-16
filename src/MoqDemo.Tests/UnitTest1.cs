f0fd65e468a48771770111b7dc73e372706c9844
Eugene Shmakov <eugene@shmakov.dev>
2022-11-16 21:24:03 +0100

    Added MoqDemo.sln

---
+++ src/MoqDemo.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MoqDemo.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
